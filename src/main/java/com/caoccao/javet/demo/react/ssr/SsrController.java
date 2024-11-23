eb6e65de2aa947c24857c9679c3df465d93328d6
Sam Cao <sjtucaocao@gmail.com>
2024-11-23 10:55:04 +0800

    ✨ feat: Add SsrController, SsrErrorController

---
+++ src/main/java/com/caoccao/javet/demo/react/ssr/SsrController.java
@@ -0,0 +1,17 @@
+package com.caoccao.javet.demo.react.ssr;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SsrController {
+    @GetMapping("/demo")
+    public String render() {
+        try {
+            return "Hello SSR!";
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            return "Error rendering component: " + e.getMessage();
+        }
+    }
+}
---
+++ src/main/java/com/caoccao/javet/demo/react/ssr/SsrErrorController.java
@@ -0,0 +1,8 @@
+package com.caoccao.javet.demo.react.ssr;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SsrErrorController implements ErrorController {
+}
